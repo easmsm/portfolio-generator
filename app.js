8e30750557d246c5107dfded7ceec5ef78b3e014
ekukwa <easmsm@gmail.com>
2022-07-19 20:14:16 -0400

    added page-template as source for html

--- app.js
+++ app.js
@@ -1,39 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const [Emily, easmsm] = profileDataArgs;
 
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
 
-//   console.log('================');
+fs.writeFile('index.html', generatePage('Emily', 'easmsm'), err => {
+  if (err) throw err;
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-const generatePage = (Emily, easmsm) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-    <h1>${Emily}</h1>
-    <h2><a href="https://github.com/${easmsm}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
-
-console.log('Emily', 'easmsm');
-console.log(generatePage('Emily', 'easmsm'));
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
